63a3691d8ef9766d64fb0f69c28dc1ceb64f59e6
daniel gordon <danielgordon19972@gmail.com>
2021-07-20 20:07:18 +0300

    added princes taverns website to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,16 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
+    origins "https://princes-tavern.herokuapp.com/"
+    resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head], credentials: true
+  end
+
+  allow do
+    origins "http://localhost:3001"
+    resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head], credentials: true
+  end
+
+  allow do
     origins "http://localhost:3000"
-    resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head], credentials: true
-  end
-
-  allow do
-    origins "http://localhost:3001"
-    resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head], credentials: true
-  end
-
-  allow do
-    origins "http://localhost:3001"
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head]
   end
 end
