d6e26e598bd065225d2c5c7be4cbd128cc0c3de3
Daniel Gordon <danielgordon19972@gmail.com>
2022-11-11 12:03:44 +0200

    Updated cors policy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://princes-tavern.herokuapp.com/"
+    origins "https://princes-tavern.netlify.app/"
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head], credentials: true
   end
 
